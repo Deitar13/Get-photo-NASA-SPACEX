ee4f2f8eb01abfc51115d8dbe799d4a8da87f03a
Deitar13 <89253911+Deitar13@users.noreply.github.com>
2022-04-06 10:31:29 +0300

    Исправления:
    Удалил часть лишнего кода.
    Отсортировал импорты.
    Изменил регистр букв в в переменных.
    Удалил лишние папки, теперь все сгружается в "Images".
    Пенерес некоторые переменные внутрь функции.
    Подсушил код.
    Убрал сравнение с пустым списком.
    Исправил число публикуемых картинок.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import datetime
-import json
 import os
 import os.path
 import random
@@ -10,9 +9,11 @@ import telegram
 
 from dotenv import load_dotenv
 from pathlib import Path
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
 from urllib.parse import urlsplit, urlparse
+
+
+def ensure_dir(file_path):
+    Path(file_path).mkdir(parents=True, exist_ok=True)
 
 
 def get_extension(url):
@@ -21,39 +22,33 @@ def get_extension(url):
     return os.path.splitext(path)[1]
 
 
-def fetch_apod_photos(nasa_url):
-    count = 30
-    payload = {"api_key": f"{nasa_api_token}", "count": f"{count}"}
+def fetch_apod_photos(file_path, nasa_api_token, count_apod_photos):
+    nasa_url = "https://api.nasa.gov/planetary/apod"
+    payload = {"api_key": f"{nasa_api_token}", "count": f"{count_apod_photos}"}
     response = requests.get(nasa_url, params=payload)
     response.raise_for_status()
-
-    file_path = "apod_images"
-    ensure_dir(file_path)
-
     urls_count = 0
+
     for item in response.json():
         parsed_url = urlparse(item['url'])
-        if parsed_url.netloc == "apod.nasa.gov":
-            urls_count += 1
-            extension = get_extension(item['url'])
-            photo_name = (f"apod{urls_count}{extension}")
-            response = requests.get(item['url'])
-            response.raise_for_status
-            with open(f"{file_path}/{photo_name}", 'wb') as file:
-                file.write(response.content)
+
+        urls_count += 1
+        extension = get_extension(item['url'])
+        photo_name = (f"apod{urls_count}{extension}")
+        response = requests.get(item['url'])
+        response.raise_for_status
+        with open(f"{file_path}/{photo_name}", 'wb') as file:
+            file.write(response.content)
 
 
-def ensure_dir(file_path):
-    Path(file_path).mkdir(parents=True, exist_ok=True)
-
-
-def fetch_spacex_last_launch(url, file_path):
+def fetch_spacex_last_launch(file_path):
+    url = 'https://api.spacexdata.com/v3/launches/'
     links_list = []
     response = requests.get(url)
     response.raise_for_status
 
     for item in response.json():
-        if item["links"]["flickr_images"] != []:
+        if item["links"]["flickr_images"]:
             links_list.append(item["links"]["flickr_images"])
     last_photos_links = links_list[-1]
 
@@ -65,7 +60,8 @@ def fetch_spacex_last_launch(url, file_path):
             file.write(response.content)
 
 
-def get_epic_earth_photos_urls(epic_url):
+def get_epic_earth_photos_urls(file_path):
+    epic_url = 'https://api.nasa.gov/EPIC/api/natural/images?api_key=DEMO_KEY'
     response = requests.get(epic_url)
     response.raise_for_status()
 
@@ -78,41 +74,32 @@ def get_epic_earth_photos_urls(epic_url):
 
 
 def download_epic_earth_photo(epic_earth_photo_url, name_of_photo):
-    file_path = "epic_earth_photo"
-    ensure_dir(file_path)
     response = requests.get(epic_earth_photo_url)
     response.raise_for_status
     with open(f"{file_path}/{name_of_photo}.png", 'wb') as file:
         file.write(response.content)
 
 if __name__ == "__main__":
-
     load_dotenv()
     nasa_api_token = os.environ["NASA_API_TOKEN"]
     telegram_api_token = os.environ["TELEGRAM_API"]
     telegram_channel_chat_id = os.environ["CHAT_ID"]
-
     file_path = "images"
+
     ensure_dir(file_path)
-
-    url = 'https://api.spacexdata.com/v3/launches/'
-
-    fetch_spacex_last_launch(url, file_path)
-
-    nasa_url = "https://api.nasa.gov/planetary/apod"
-    fetch_apod_photos(nasa_url)
-
-    epic_url = 'https://api.nasa.gov/EPIC/api/natural/images?api_key=DEMO_KEY'
-    get_epic_earth_photos_urls(epic_url)
+    fetch_spacex_last_launch(file_path)
+    count_apod_photos = 5
+    fetch_apod_photos(file_path, nasa_api_token, count_apod_photos)
+    get_epic_earth_photos_urls(file_path)
 
     while True:
 
-        time_period = 1
+        time_period = 86400
         bot = telegram.Bot(token=f'{telegram_api_token}')
         print(bot.get_me())
-        apod_images = os.listdir('apod_images')
-        random_apod_image = random.choice(apod_images)
+        images = os.listdir('images')
+        random_image = random.choice(images)
 
-        print(random_apod_image)
-        bot.send_document(chat_id=telegram_channel_chat_id, document=open(f'apod_images/{random_apod_image}', 'rb'))
+        print("опубликовать данную картинку:", random_image)
+        bot.send_document(chat_id=telegram_channel_chat_id, document=open(f'images/{random_image}', 'rb'))
         time.sleep(time_period)
